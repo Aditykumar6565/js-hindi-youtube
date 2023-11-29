2749d61f4839ce9f38d51e0bf7dbb07cdd2a90e0
Aditykumar6565 <86483020+Aditykumar6565@users.noreply.github.com>
2023-11-29 10:50:09 +0000

    datatypesummary

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,35 @@
+//Primitive
+//7 
+/* String 
+Number
+Boolean
+null
+undefined
+Symbol
+Big Int
+
+*/
+//Reference Non-primitive
+//If you want to master javascript master object arrays browser event 
+/*
+Arrays,
+objects
+Function
+
+ */
+
+/*
+ ? JavaScript is dynamically typed or statically typed????
+
+*/
+
+const score= 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+let useEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
