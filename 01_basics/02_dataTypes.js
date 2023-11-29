53953d47fa3966bb783819cc507c21d36432eaa8
Aditykumar6565 <86483020+Aditykumar6565@users.noreply.github.com>
2023-11-29 10:10:25 +0000

    commit changes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) we are using node js not as document or console 
+
+console.log(3+2) ;
+//code readability is important 
+console.log("Aditya");
+/*for documentaion use https://tc39.es/ecma262/sec-intro;;  
+and mdn*/
+
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalon value 
+//undefined=>
+
+// object 
+
+console.log(typeof null)// object
+console.log(typeof undefined) // undefined
+let age = 25;
+let name = "aditya"
+let isloggedIn = false;
+console.log(typeof age);/* if we define it above the line 23 it will not be recognized */
+
+//
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "hitesh";
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//"33"=>33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isloggedIn = "hitesh"
+let booleanIsLoggedIn = Number(isloggedIn)
+//console.log(typeof booleanIsLoggedIn)
+
+//1 => true; 0=> false
+//"" => flase
+//"hitesh"=> true
+
+//**********************Opeartion ************************/
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2;
+console.log(str3);
