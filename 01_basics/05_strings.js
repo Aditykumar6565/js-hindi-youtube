286bf03060ea6ff614f3ae61e9948c8f3fb61a63
Aditykumar6565 <86483020+Aditykumar6565@users.noreply.github.com>
2023-11-30 08:18:35 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "aditya"
+const repoCount = 100
+
+console.log(`Heloo i am ${name} and my reocount is ${repoCount} `);
+const gameName = new String('Aditya-ku-mar')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'));
+
+//const newString = gameName.substring(0,4);
+//console.log(newString);
+
+//const anotherString = gameName.slice(-2,4);
+//console.log(anotherString);
+
+const newString = "  hitesh  "
+console.log(newString);
+console.log(newString.trim());
+
+const url = "https//aditya.com/aditya%20Kumar";
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
