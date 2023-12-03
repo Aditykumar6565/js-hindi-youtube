31e68fc85ed08393e422a13f8de5a543160c0962
Aditykumar6565 <86483020+Aditykumar6565@users.noreply.github.com>
2023-12-03 06:43:19 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("A");
+    console.log("D");
+    console.log("I");
+    console.log("T");
+    console.log("Y");
+    console.log("A");
+}
+//sayMyName()
+
+function addTwoNumber(num1,num2) {
+     console.log(num1+num2);
+    
+}
+const result = addTwoNumber(3,4)
+console.log(result);
